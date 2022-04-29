a5a39d8d320849def2e2b3950c48c5a01ef38d3e
JOMA-OFFICE <jomadelema@hotmail.com>
2022-04-29 16:07:51 -0600

    se creo el layout y se corrigio el approuter

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -8,25 +8,30 @@ import ProjectPage from "../pages/ProjectPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import UsersPage from "../pages/admin/UsersPage";
 
+import Navigation from "../components/Navigation";
+import Layout from "../components/layouts/Layout";
+
 export default function AppRouter() {
-  return <>
-  <Router>
-      <Routes>
-        <Route exact path="/" element={<HomePage />}/>
-        <Route exact path="/login" element={<LoginPage />}/>
-        <Route exact path="/register" element={<RegisterPage />}/>
-        <Route exact path="/account" element={<AccountPage />}/>
-        <Route exact path="/projects" element={<ProjectsPage />}/>
-        <Route exact path="/project/:projectId" element={<ProjectPage />}/>
-        <Route exact path="/admin/users" element={<UsersPage />}/>
+  return (
+    <>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route exact path="/register" element={<RegisterPage />} />
+            <Route exact path="/account" element={<AccountPage />} />
+            <Route exact path="/projects" element={<ProjectsPage />} />
+            <Route exact path="/project/:projectId" element={<ProjectPage />} />
+            <Route exact path="/admin/users" element={<UsersPage />} />
 
-        <Route path="*" element={<NotFoundPage/>}/>
-      </Routes>
-  </Router>
-  
-  </>;
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </>
+  );
 }
-
 
 //Ejemplo de uso de rutas
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
