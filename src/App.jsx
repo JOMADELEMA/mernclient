8eca2b8eb60418898ce57745e3efc4fd63d419a5
JOMA-OFFICE <jomadelema@hotmail.com>
2022-04-29 12:30:24 -0600

    primer commit, ya se avanzo con estructura de proyecto rutas y paginas

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import AppRouter from './routers/AppRouter';
+
+function App() {
+  return (
+    <>
+    <AppRouter />
+    <h1>hola</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import AccountPage from "../pages/AccountPage";
+import ProjectsPage from "../pages/ProjectsPage";
+import ProjectPage from "../pages/ProjectPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import UsersPage from "../pages/admin/UsersPage";
+
+export default function AppRouter() {
+  return <>
+  <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />}/>
+        <Route exact path="/login" element={<LoginPage />}/>
+        <Route exact path="/register" element={<RegisterPage />}/>
+        <Route exact path="/account" element={<AccountPage />}/>
+        <Route exact path="/projects" element={<ProjectsPage />}/>
+        <Route exact path="/project/:projectId" element={<ProjectPage />}/>
+        <Route exact path="/admin/users" element={<UsersPage />}/>
+
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+  </Router>
+  
+  </>;
+}
+
+
+//Ejemplo de uso de rutas
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// <Router>
+//   <Routes>
+//     <Route exact path="/" element={<Index />} />
+//     <Route path="/aboutus" element={<AboutMe />} />
+//     <Route path="/contactus" element={<Contacto />} />
+//     <Route path="/proyectos" element={<Proyectos />} />
+//     <Route path="/shieldui" element={<ShieldUI />} />
+//   </Routes>
+// </Router>;
