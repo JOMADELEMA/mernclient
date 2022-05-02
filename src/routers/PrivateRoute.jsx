6115dd7c6b53cfd74818984375b75bcbb52f9938
JOMADELEMA <jomadelema@gmail.com>
2022-05-01 23:44:37 -0600

    se agrego isAuth e isRole a AuthProvider.

--- src/routers/PrivateRoute.jsx
+++ src/routers/PrivateRoute.jsx
@@ -3,16 +3,16 @@ import { Navigate } from "react-router-dom";
 import useAuth from "../components/auth/useAuth";
 
 const PrivateRoute = (props) => {
-  const {user} = useAuth();
+  const {isAuth, isRole} = useAuth();
   const {hasRole: role, children} = props;
 
 
-  console.log(props)
-  console.log(user.role)
+  // console.log(props)
+  // console.log(user.role)
   // console.log(children)
 
-  if (role && user?.role !== role ) return <Navigate to="/" />;
-  if (!user) return <Navigate to="/login" />;
+  if (role && !isRole(role)) return <Navigate to="/" />;
+  if (!isAuth) return <Navigate to="/login" />;
   return children;
 
   //   return <Route {...props} />;
