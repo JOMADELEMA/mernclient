ff1b32c4a3495cee6d71b9c36123cd1770c00d0b
JOMADELEMA <jomadelema@gmail.com>
2022-05-11 23:52:08 -0600

    se implementó glassmorfism

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -3,11 +3,11 @@ import {
   Container,
   Row,
   Col,
-  DropdownButton,
-  Dropdown,
+  Button
 } from "react-bootstrap";
 import axios from "axios";
 import useAuth from "../components/auth/useAuth";
+import { Plus } from "tabler-icons-react";
 
 import "./ProjectsPage.css";
 
@@ -25,8 +25,6 @@ const ProjectsPage = () => {
         id_usuario: user.id_usuario,
       })
       .then((respuesta) => {
-        // console.log("acá está la respuesta");
-        // console.log(respuesta.data.data);
         setPosts(respuesta.data.data);
       })
       .catch((error) => {
@@ -45,46 +43,53 @@ const ProjectsPage = () => {
     <>
       <Container>
         <Row>
-          <Col>
-            <h1>
-              ProjectsPage
-            </h1>
+          <Col className="contenedor-ui-proyectos">
+            <div className="titulo w-100">
+              <h1>
+                ProjectsPage
+              </h1>
 
-            <button className="btn btn-primary" onClick={() => obtenerDatos()}>
-              Agregar Post
-            </button>
+              <Plus size="48px" color="white" className="boton-agregar-post" onClick={() => obtenerDatos()} />
 
-            {posts?.length ? (
-              posts.map((item) => (
-                <div className="contenedor-tarjeta-proyecto">
-                  <div className="encabezado-tarjeta-proyecto">
-                    <h4>{item.id_post}</h4>
-                  </div>
-                  <div className="cuerpo-tarjeta-proyecto">
-                    <div className="texto-tarjeta">{item.texto}</div>
-                    <div className="menu-tarjeta">
-                      <DropdownButton
-                        variant="none"
-                        style={{ otuline: "none" }}
-                        title="&#8226;&#8226;&#8226;"
-                      >
-                        <Dropdown.Item>Ver</Dropdown.Item>
+            </div>
 
-                        <Dropdown.Item>Editar</Dropdown.Item>
+            <div className="contenedor-tarjetas">
 
-                        <Dropdown.Item>Eliminar</Dropdown.Item>
-                      </DropdownButton>
+              {posts?.length ? (
+                posts.map((item) => (
+                  <div className="contenedor-tarjeta-proyecto">
+                    <div className="encabezado-tarjeta-proyecto">
+                      <h4>{item.id_post}</h4>
+                    </div>
+                    <div className="cuerpo-tarjeta-proyecto">
+                      <div className="texto-tarjeta">{item.texto}</div>
+                      <div className="menu-tarjeta">
+                        <div className="dropdown"
+                        >
+                          <a href="#" className="boton-dropdown" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" >
+                            &#8226;&#8226;&#8226;
+                          </a>
+                          <ul className="dropdown-menu text-center" aria-labelledby="dropdownMenuLink">
+                            <li className="dropdown-item">Ver</li>
+                            <li className="separador-dropdown-menu"></li>
+                            <li className="dropdown-item">Editar</li>
+                            <li className="separador-dropdown-menu"></li>
+                            <li className="dropdown-item">Eliminar</li>
+                          </ul>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))
-            ) : (
-              <h1>No hay post para mostrar</h1>
-            )}
+                ))
+              ) : (
+                <h1>No hay post para mostrar</h1>
+              )}
+            </div>
           </Col>
+
         </Row>
 
-        <Row>
+        {/* <Row>
           <Col>
             <div className="contenedor-tarjeta-proyecto">
               <div className="encabezado-tarjeta-proyecto">
@@ -93,22 +98,22 @@ const ProjectsPage = () => {
               <div className="cuerpo-tarjeta-proyecto">
                 <div className="texto-tarjeta">Descripcion....</div>
                 <div className="menu-tarjeta">
-                  <DropdownButton
-                    variant="none"
-                    style={{ otuline: "none" }}
-                    title="&#8226;&#8226;&#8226;"
+                  <div className="dropdown"
                   >
-                    <Dropdown.Item>Ver</Dropdown.Item>
-
-                    <Dropdown.Item>Editar</Dropdown.Item>
-
-                    <Dropdown.Item>Eliminar</Dropdown.Item>
-                  </DropdownButton>
+                    <a href="#" className="boton-dropdown" id="dropdownMenuLink" role="button" data-bs-toggle="dropdown" >
+                      &#8226;&#8226;&#8226;
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                      <li className="dropdown-item">Ver</li>
+                      <li className="dropdown-item">Editar</li>
+                      <li className="dropdown-item">Eliminar</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
           </Col>
-        </Row>
+        </Row> */}
       </Container>
     </>
   );
